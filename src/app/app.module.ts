99feb7053366c8f19c216b9fce0acfe303d3a1f5
Andressa Reis Araujo <andressareis98@outlook.com>
2021-08-30 11:12:15 -0300

    feat: create photo board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 import { PhotoFrameModule } from './shared/photo-frame/photo-frame.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,8 @@ import { LikeWidgetModule } from './shared/components/like-widget/like-widget.mo
     AppRoutingModule,
     LikeWidgetModule,
     PhotoFrameModule,
+    HttpClientModule,
+    PhotoBoardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
