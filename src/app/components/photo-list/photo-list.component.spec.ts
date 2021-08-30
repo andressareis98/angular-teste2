d6911798e0739969943c0ccf2d4f91e87962a345
Andressa Reis Araujo <andressareis98@outlook.com>
2021-08-30 17:16:05 -0300

    feat: test service

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,20 +1,61 @@
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListModule } from './photo-list.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
+import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
+import { of } from 'rxjs';
 
-describe('PhotoListComponent', () => {
+function buildPhotoList(): Photo[] {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: '',
+    });
+  }
+  return photos;
+}
+
+describe(PhotoListComponent.name, () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
+  let service: PhotoBoardService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PhotoListComponent],
+      imports: [PhotoListModule, HttpClientModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(PhotoBoardService);
+  });
+
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`(D) Should display board when data arrives`, () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(of(photos));
     fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).not.toBeNull();
+    expect(loader).toBeNull();
+  });
+
+  it(`(D) Should display loader while waiting for data`, () => {
+    spyOn(service, 'getPhotos').and.returnValue(of(null));
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(loader).not.toBeNull();
+    expect(board).toBeNull();
   });
 });
--- src/app/components/photo-list/photo-list.module.ts
+++ src/app/components/photo-list/photo-list.module.ts
@@ -1,3 +1,4 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PhotoBoardModule } from './../../shared/components/photo-board/photo-board.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,7 +6,7 @@ import { PhotoListComponent } from './photo-list.component';
 
 @NgModule({
   declarations: [PhotoListComponent],
-  imports: [CommonModule, PhotoBoardModule],
+  imports: [CommonModule, PhotoBoardModule, FontAwesomeModule],
   exports: [PhotoListComponent],
 })
 export class PhotoListModule {}
